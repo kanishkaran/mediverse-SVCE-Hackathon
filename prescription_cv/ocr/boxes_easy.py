158618f18bf396b2b84f1081e3107080b8f1ca08
raven <tshreeram16@gmail.com>
2024-03-26 20:38:47 +0530

    Update boxes_easy.py

--- prescription_cv/ocr/boxes_easy.py
+++ prescription_cv/ocr/boxes_easy.py
@@ -30,7 +30,7 @@ def detect_and_display_text(image_path):
     cv2.destroyAllWindows()
 
 # Path to your handwritten image
-image_path = r'D:\code\hackathon\word_recog\doc_digit.jpg'
+image_path = r'image path goes here'
 
 # Detect and display handwritten text with bounding boxes
 detect_and_display_text(image_path)
