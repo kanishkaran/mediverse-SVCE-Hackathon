a9968fab9aa4a9631ff2728b256e581af5b7a59a
tharun <88714069+tharun52@users.noreply.github.com>
2024-04-06 14:07:17 +0530

    Update custom_filters.py

--- mediverse/templatetags/custom_filters.py
+++ mediverse/templatetags/custom_filters.py
@@ -1,5 +1,4 @@
 from django import template
-import json
 import ast
 
 register = template.Library()
@@ -9,5 +8,11 @@ def parse_list(value):
         return ast.literal_eval(value)
     except (ValueError, SyntaxError):
         return value
+def convert_to_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
 
+register.filter('convert_to_int', convert_to_int)
 register.filter('parse_list', parse_list)
